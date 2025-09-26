aff60a70566e06702f3369eb5da19845b75b76f1
anerudoanderson <anerudoanderson@gmail.com>
2025-09-26 09:50:14 +0200

    Update Question 5.py

--- Question 5.py
+++ Question 5.py
@@ -1,10 +1,5 @@
-# Write a Python program that uses a lambda function and the map function to
-# convert a list of temperatures in Celsius to Fahrenheit. The formula for conversion is
-# F = C * 9/5 + 32. Include a sample list of Celsius temperatures and print the
-# converted list.
-
 # Sample list of temperatures in Celsius
-celsius_temps = [0, 15, 25, 35, 43, 100]
+celsius_temps = [10, 25, 35, 45, 53, 60]
 
 # Convert to Fahrenheit using map and a lambda function
 fahrenheit_temps = list(map(lambda c: c * 9/5 + 32, celsius_temps))
