a7957fcd9547ab6f6136345954da8a59c0eef2bc
anerudoanderson <anerudoanderson@gmail.com>
2025-09-26 09:50:38 +0200

    Update Question 6.py

--- Question 6.py
+++ Question 6.py
@@ -1,10 +1,3 @@
-# Create a function called divide_numbers that takes two parameters, numerator and
-# denominator. Use a try block to handle:
-# Division by zero (ZeroDivisionError).
-# Invalid input types (TypeError).
-# The function should return the result of the division if successful, and print an
-# appropriate error message for each exception.
-
 def divide_numbers(numerator, denominator):
     try:
         result = numerator / denominator
