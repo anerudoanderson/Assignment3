fc3dc3963fee2631acf2c731004b17fe900cf27d
anerudoanderson <anerudoanderson@gmail.com>
2025-09-26 09:48:54 +0200

    Update Question 4.py

--- Question 4.py
+++ Question 4.py
@@ -1,10 +1,5 @@
-# Create a Python script that writes a list of names to a file called names.txt. Each
-# name should be on a new line. Then, read the file and print each name to the
-# console. Use the with statement to handle file operations and ensure the file is
-# properly closed.
-
 # List of names to write to the file
-names = ["Nomagugu", "Nathan", "Munashe", "Audry", "Kudzai"]
+names = ["Sam", "Anerudo", "Anderson", "Zivai", "Thompson"]
 
 # Write names to 'names.txt', each on a new line
 with open("names.txt", "w") as file:
